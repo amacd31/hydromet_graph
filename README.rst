a86efa4724a5b6dcb8635a8bf81d4f6a661c9e7a
Andrew MacDonald <andrew@maccas.net>
2015-01-31 12:45:09 +1100

    Initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+Hydromet Graph
+===============
+
+Graphing library for hydrological and meteorological variables.
+
+Dependencies
+------------
+
+Requires Python 2.6 or greater (mostly tested with Python 3.3 on Linux), numpy and pandas.
+
+
+Usage
+-----
+
+ TODO
+
---
+++ setup.py
@@ -0,0 +1,47 @@
+import os
+from io import open
+
+import versioneer
+versioneer.versionfile_source = 'hydromet_graph/_version.py'
+versioneer.versionfile_build = 'hydromet_graph/_version.py'
+versioneer.tag_prefix = 'v'
+versioneer.parentdir_prefix = 'hydromet_graph-'
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = ''.join([
+            line for line in f.readlines() if 'travis-ci' not in line
+        ])
+
+setup(
+    name='hydromet_graph',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='Graphing library for hydrological and meteorological variables.',
+    long_description=long_description,
+    author='Andrew MacDonald',
+    author_email='andrew@maccas.net',
+    license='BSD',
+    url='https://github.com/amacd31/hydromet_graph',
+    install_requires=['numpy', 'pandas'],
+    packages = ['hydromet_graph'],
+    test_suite = 'tests',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
